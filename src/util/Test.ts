54eb1337f006c1e44826309d80c79e78ca28725e
HADMARINE <tsx@kakao.com>
2021-06-11 06:51:26 +0900

    Update tests: resolved no exit on test finish

--- src/util/Test.ts
+++ src/util/Test.ts
@@ -1,6 +1,7 @@
 import { Root } from '../index';
 import http from 'http';
 import ServerBuilder from 'express-quick-builder';
+import mongoose from 'mongoose';
 
 let rootInstance: ReturnType<typeof ServerBuilder['serverStarter']> | undefined;
 
@@ -16,6 +17,7 @@ function closeServer(
 ): void {
   if (!rootInstance) throw new Error('Root instance is not initialized!');
   rootInstance.server.close();
+  mongoose.connection.close();
   rootInstance = undefined;
   done();
 }
