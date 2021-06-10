4a8b3515effee13cc4445f07760e7282ce6f0c03
HADMARINE <tsx@kakao.com>
2021-06-11 06:51:47 +0900

    Update time controller

--- src/routes/v1/time.controller.ts
+++ src/routes/v1/time.controller.ts
@@ -2,6 +2,7 @@ import Time from '@models/Time';
 import { Controller, GetMapping } from 'express-quick-builder';
 
 interface AdminTimeControllerInterface {
+  getAllTime(): Promise<{ hackathon: Date; market: Date } | null>;
   hackathonTime(): Promise<Date | null>;
   marketTime(): Promise<Date | null>;
 }
@@ -10,6 +11,17 @@ interface AdminTimeControllerInterface {
 export default class AdminTimeController
   implements AdminTimeControllerInterface
 {
+  @GetMapping('/start')
+  async getAllTime(): Promise<{ hackathon: Date; market: Date } | null> {
+    const hackathon = await Time.findOne({ type: 'hackathon' }).exec();
+    const market = await Time.findOne({ type: 'market' }).exec();
+    if (!hackathon || !market) return null;
+    return {
+      hackathon: new Date(hackathon.value),
+      market: new Date(market.value),
+    };
+  }
+
   @GetMapping('/start/hackathon')
   async hackathonTime(): Promise<Date | null> {
     const time = await Time.findOne({ type: 'hackathon' }).exec();
