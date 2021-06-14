8045c54f32fd5fa584a3ea30f7fea22c062a24ad
HADMARINE <tsx@kakao.com>
2021-06-14 22:36:40 +0900

    Update aws scripts

--- .aws/scripts/start.sh
+++ .aws/scripts/start.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/app
-curl https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+curl -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
 yarn install
 yarn build
 pm2 start
