2a132ab79ca7767142ad6142b4873f126072f32a
HADMARINE <tsx@kakao.com>
2021-06-16 08:59:01 +0900

    Update time controller

--- src/routes/v1/time.controller.ts
+++ src/routes/v1/time.controller.ts
@@ -1,20 +1,23 @@
 import Time from '@models/Time';
 import { Controller, GetMapping } from 'express-quick-builder';
 
-interface AdminTimeControllerInterface {
-  getAllTime(): Promise<{ hackathon: Date; market: Date } | null>;
-  hackathonTime(): Promise<Date | null>;
-  marketTime(): Promise<Date | null>;
-}
-
 @Controller
-export default class AdminTimeController
-  implements AdminTimeControllerInterface
-{
+export default class AdminTimeController {
   @GetMapping('/start')
-  async getAllTime(): Promise<{ hackathon: Date; market: Date } | null> {
-    const hackathon = await Time.findOne({ type: 'hackathon' }).exec();
-    const market = await Time.findOne({ type: 'market' }).exec();
+  async getAllStartTime(): Promise<{ hackathon: Date; market: Date } | null> {
+    const hackathon = await Time.findOne({ type: 'hackathon-start' }).exec();
+    const market = await Time.findOne({ type: 'market-start' }).exec();
+    if (!hackathon || !market) return null;
+    return {
+      hackathon: new Date(hackathon.value),
+      market: new Date(market.value),
+    };
+  }
+
+  @GetMapping('/end')
+  async getAllEndTime(): Promise<{ hackathon: Date; market: Date } | null> {
+    const hackathon = await Time.findOne({ type: 'hackathon-end' }).exec();
+    const market = await Time.findOne({ type: 'market-end' }).exec();
     if (!hackathon || !market) return null;
     return {
       hackathon: new Date(hackathon.value),
@@ -23,14 +26,26 @@ export default class AdminTimeController
   }
 
   @GetMapping('/start/hackathon')
-  async hackathonTime(): Promise<Date | null> {
-    const time = await Time.findOne({ type: 'hackathon' }).exec();
+  async hackathonStartTime(): Promise<Date | null> {
+    const time = await Time.findOne({ type: 'hackathon-start' }).exec();
     return time ? time.value : null;
   }
 
   @GetMapping('/start/market')
+  async marketStartTime(): Promise<Date | null> {
+    const time = await Time.findOne({ type: 'market-start' }).exec();
+    return time ? time.value : null;
+  }
+
+  @GetMapping('/end/hackathon')
+  async hackathonTime(): Promise<Date | null> {
+    const time = await Time.findOne({ type: 'hackathon-end' }).exec();
+    return time ? time.value : null;
+  }
+
+  @GetMapping('/end/market')
   async marketTime(): Promise<Date | null> {
-    const time = await Time.findOne({ type: 'market' }).exec();
+    const time = await Time.findOne({ type: 'market-end' }).exec();
     return time ? time.value : null;
   }
 }
