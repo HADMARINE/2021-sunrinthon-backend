1c37fc157d49595e28dc5b126a821611525930ef
HADMARINE <tsx@kakao.com>
2021-07-10 22:29:11 +0900

    Create PassedTeams model

---
+++ src/models/PassedTeams.ts
@@ -0,0 +1,36 @@
+import { model, Schema, Document, HookNextFunction, models } from 'mongoose';
+import error from '@error/ErrorDictionary';
+
+export interface PassedTeamsInterface {
+    name: string;
+    field: "게임" | "생활";
+}
+
+const PassedTeamsSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    field: { type: String, required: true }
+});
+
+export interface PassedTeamsDocument extends Document, PassedTeamsInterface {
+    // Add Methods here
+}
+
+// PassedTeamsSchema.methods.~~
+
+PassedTeamsSchema.pre('save', function (next: HookNextFunction): void {
+    const doc = this as PassedTeamsDocument;
+    models.PassedTeams.findOne(
+        {
+            $or: [{ name: doc.name, field: doc.field }],
+        },
+        function (err: Error, site: PassedTeamsDocument) {
+            if (site) next(error.db.exists());
+            if (err) next(err);
+            next();
+        },
+    );
+});
+
+const PassedTeams = model<PassedTeamsDocument>('PassedTeams', PassedTeamsSchema);
+
+export default PassedTeams;
