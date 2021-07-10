c0439857b429bd7bb246f3c092da3aa74a560c30
HADMARINE <tsx@kakao.com>
2021-07-10 22:54:43 +0900

    Update passed controller - add api not yet

--- src/routes/v1/teams/passed.controller.ts
+++ src/routes/v1/teams/passed.controller.ts
@@ -1,11 +1,16 @@
 import ErrorDictionary from '@error/ErrorDictionary';
 import PassedTeams from '@models/PassedTeams';
+import Time from '@models/Time';
 import { Controller, GetMapping } from 'express-quick-builder';
 
 @Controller
 export default class PassedTeamsController {
   @GetMapping()
   async getPassedTeams(): Promise<{ game: string[]; living: string[] } | null> {
+    const time = await Time.findOne({ type: 'announce_team-start' });
+    if (time.value.getTime() > Date.now()) {
+      throw ErrorDictionary.rule.apiNotYet();
+    }
     const teams = await PassedTeams.find();
     if (!teams.length) {
       return null;
