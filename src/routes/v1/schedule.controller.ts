71199d3e8877645ea0e1fd1f00b636e79afdf8c6
HADMARINE <tsx@kakao.com>
2021-07-16 11:51:41 +0900

    Update schedule

---
+++ src/routes/v1/schedule.controller.ts
@@ -0,0 +1,17 @@
+import Setting from '@models/Setting';
+import Aws from '@util/Aws';
+import { Controller, GetMapping } from 'express-quick-builder';
+
+@Controller
+class ScheduleController {
+  @GetMapping()
+  async getScheduleImage(): Promise<string | null> {
+    const scheduleValue: string | undefined = (
+      await Setting.findOne({ key: 'schedule-image-location' })
+    )?.value;
+    if (!scheduleValue) return null;
+    const schedule: { Key: string; Bucket: string } = JSON.parse(scheduleValue);
+
+    return await Aws.S3.getSignedUrl({ ...schedule });
+  }
+}
